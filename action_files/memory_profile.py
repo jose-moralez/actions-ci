95e58a1e365dcd79cba1cedcff68096d367ed4cb
José Morales <jmorales@grupoabraxas.com>
2021-01-28 16:40:57 -0600

    agregar accion comentario

---
+++ action_files/memory_profile.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+from mimod.frecuencia import frecuencia
+
+
+if __name__ == '__main__':
+    arreglo_grande = np.random.randint(0, 1_000, size=100_000_000) # ~ 760MB
+    for i in range(10):
+        frecuencia(arreglo_grande, i)
